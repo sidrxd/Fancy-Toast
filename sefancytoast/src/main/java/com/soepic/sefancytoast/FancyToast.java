a2899a2951561b43cc931d1b41f6b39a0644a331
Siddharth Shakya <sidrxd@gmail.com>
2020-11-11 16:22:37 +0530

    Added Lib

---
+++ sefancytoast/src/main/java/com/soepic/sefancytoast/FancyToast.java
@@ -0,0 +1,29 @@
+package com.soepic.sefancytoast;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FancyToast {
+
+
+    public static void fancyToast(Context context, String msg, int toastImg) {
+        View v = LayoutInflater.from(context).inflate(R.layout.toast, null);
+        TextView toast = v.findViewById(R.id.toast_text);
+        toast.setText(msg);
+
+        ImageView imageView = v.findViewById(R.id.toast_img);
+        imageView.setImageResource(toastImg);
+
+        Toast myToast = new Toast(context);
+
+        myToast.setView(v);
+        myToast.setDuration(Toast.LENGTH_SHORT);
+        myToast.show();
+
+    }
+
+}
